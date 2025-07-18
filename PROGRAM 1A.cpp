e8e371d8c73d1d494a8ed660493345b9342d0aa5
yashshrivastava972 <yashshrivastava578@gmail.com>
2025-07-18 10:43:49 +0530

    Create PROGRAM 1A.cpp

---
+++ PROGRAM 1A.cpp
@@ -0,0 +1,19 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: Hello World Program (EXPERIMENT: 1A)
+
+
+// CODE:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
+
+// OUTPUT:
+
+Hello World
