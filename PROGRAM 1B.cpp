fc5b8f8dbe3bdaa8ee86b4121e81c73fd0d68ba0
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-20 16:46:22 +0530

    Update PROGRAM 1B.cpp

--- PROGRAM 1B.cpp
+++ PROGRAM 1B.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float num1, num2, sum=0, diff, prod, div;
+    cout<<"ENTER FIRST NUMBER:";
+    cin>>num1;
+    cout<<"ENTER SECOND NUMBER:";
+    cin>>num2;
+    
+    sum = num1+num2;
+    cout<<"SUM OF GIVEN NUMBERS:"<<sum<<endl;
+    diff = num1-num2;
+    cout<<"DIFFERENCE OF THE NUMBERS:"<<diff<<endl;
+    prod = num1*num2;
+    cout<<"PRODUCT OF TWO NUMBERS:"<<prod<<endl;
+    div = num1/num2;
+    cout<<"DIVISION OF TWO NUMBERS:"<<div<<endl;
+    
+    return 0;
+    
+}
 
+// OUTPUT:
+// ENTER FIRST NUMBER:4
+// ENTER SECOND NUMBER:2
+// SUM OF GIVEN NUMBERS:6
+// DIFFERENCE OF THE NUMBERS:2
+// PRODUCT OF TWO NUMBERS:8
+// DIVISION OF TWO NUMBERS:2
